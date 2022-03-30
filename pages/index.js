6b1fc5d0439c9dcef3b054303676409a7a19fc37
Colt Thatcher <colt.thatcher303@gmail.com>
2022-03-30 11:44:30 -0700

    continue along with next.js practice, have linked index.js, first-post.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <main>
         <h1 className="title">
           Read{' '}
-          <Link href="https://nextjs.org">
+          <Link href="/posts/first-post">
             <a>This page!</a>
           </Link>
         </h1>
