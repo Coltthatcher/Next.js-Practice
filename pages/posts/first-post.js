2eff0336a2a97161467a57a67f689aa933121083
Colt Thatcher <colt.thatcher303@gmail.com>
2022-03-30 13:14:36 -0700

    start creating styled components for the page

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,12 @@
 import Link from 'next/link'
 import Head from 'next/head'
-import Script from 'next/script'
+// import Script from 'next/script'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
   return (
-    <>
+
+    <Layout>
     <Head>
       <title>First Post</title>
     </Head>
@@ -15,6 +17,6 @@ export default function FirstPost() {
       </Link>
     </h2>
     
-    </>
+    </Layout>
   )
 }
